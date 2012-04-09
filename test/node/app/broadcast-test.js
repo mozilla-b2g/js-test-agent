6e8b7ed4cee0dddca52ac515c490b84e487804fc
James Lal <james@lightsofapollo.com>
2012-04-09 16:01:22 -0700

    Adding broadcast app enhancement

---
+++ test/node/app/broadcast-test.js
@@ -0,0 +1,68 @@
+var Broadcast = require('../../../lib/node/app/broadcast'),
+    Responder = require('../../../lib/test-agent/responder').TestAgent.Responder;
+
+describe("node/app/broadcast", function(){
+
+  var subject, server, connectionSocket,
+      factory = require('../factory/websocket');
+
+  beforeEach(function(){
+    server = {
+      socket: new Responder(),
+      responder: new Responder()
+    };
+
+    connectionSocket = new Responder();
+    subject = new Broadcast();
+
+    subject.enhance(server);
+  });
+
+  describe(".broadcast", function(){
+    var calledWith = [];
+
+    beforeEach(function(){
+      calledWith = [];
+      subject.pool.broadcast = function(){
+        calledWith.push(arguments);
+      };
+
+      server.broadcast('test');
+    });
+
+    it("should delegate to .pool broadcast", function(){
+      expect(calledWith[0][0]).to.be('test');
+    });
+
+  });
+
+  describe("when a client connects", function(){
+
+    var socket;
+
+    beforeEach(function(){
+      socket = factory.websocket();
+
+      expect(subject.pool.has(socket)).to.be(false);
+
+      server.socket.emit('connection', socket);
+    });
+
+    it("should add socket to pool", function(){
+      expect(subject.pool.has(socket)).to.be(true);
+    });
+
+    describe("when a client 'closes'", function(){
+      beforeEach(function(){
+        socket.emit('close', socket);
+      });
+
+      it("should be removed from pool", function(){
+        expect(subject.pool.has(socket)).to.be(false);
+      });
+
+    });
+
+  });
+
+});
