9f61fe2e5295d73c85321aee56085c94020738ae
James Lal <james@lightsofapollo.com>
2012-04-10 08:38:56 -0700

    Added mocha-test-events app enhancement which will delegate to reporter to show nice reports in your console

---
+++ test/node/app/mocha-test-events-test.js
@@ -0,0 +1,59 @@
+var Mocha = require_lib('node/app/mocha-test-events'),
+    AppResponder = require_lib('node/app/responder'),
+    Reporter = require_lib('node/mocha/reporter'),
+    Responder = require_lib('test-agent/responder'),
+    //does nothing
+    MochaReporter = function(){};
+
+describe("node/app/mocha-test-events", function(){
+
+  var server,
+      subject,
+      factory = require('../factory/websocket-server');
+
+  beforeEach(function(){
+    subject = new Mocha({
+      reporterClass: MochaReporter
+    });
+
+    server = factory.websocketServer();
+
+    subject.enhance(server);
+    (new AppResponder()).enhance(server);
+  });
+
+  describe("initialization", function(){
+
+    it("should have a reporter", function(){
+      expect(subject.reporter).to.be.ok();
+    });
+
+    it("should pass through options to the Reporter", function(){
+      expect(subject.reporter.reporterClass).to.be(MochaReporter);
+    });
+  });
+
+  describe("on test data", function(){
+    var data, socket, startCalledWith;
+
+    beforeEach(function(){
+      expect(subject.reporter.proxy).not.to.be.ok();
+
+      server.responder.on('test runner', function(runner){
+        startCalledWith = runner;
+      });
+
+      data = ['start', {total: 20}];
+      server.responder.emit('test data', data);
+    });
+
+    it("should bubble up start evet on runner to test runner event on server", function(){
+      expect(startCalledWith).to.be(subject.reporter);
+    });
+
+    it("should start proxy on runner", function(){
+      expect(subject.reporter.proxy).to.be.ok();
+    });
+
+  });
+});
