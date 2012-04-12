0cb33d810d806dbeebaf055a7e355984bc37e409
James Lal <james@lightsofapollo.com>
2012-04-11 18:40:11 -0700

    Updating local .js-test-agent.js

--- .js-test-agent.js
+++ .js-test-agent.js
@@ -1,13 +1,16 @@
-var Apps = require(__dirname + '/lib/node/app/index'),
-    Suite = require(__dirname + '/lib/node/suite');
+var Apps = require(__dirname + '/lib/node/server/index'),
+    Suite = require(__dirname + '/lib/node/suite'),
+    suite = new Suite({
+      path: __dirname,
+      testDir: 'test/test-agent',
+      libDir: 'lib/test-agent'
+    });
 
 server.use(Apps.Responder).
+       use(Apps.Suite, suite).
        use(Apps.Broadcast).
        use(Apps.MochaTestEvents).
        use(Apps.RunnerGrowl).
-       use(Apps.Watcher, new Suite({
-          path: __dirname,
-          testDir: 'test/test-agent',
-          libDir: 'lib/test-agent'
-       }));
+       use(Apps.StartTests).
+       use(Apps.Watcher);
 
