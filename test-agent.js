0a8db87637d34d7e73c1aff78c3f19f6afc42363
James Lal <james@lightsofapollo.com>
2013-01-29 11:02:12 -0800

    browser build

--- test-agent.js
+++ test-agent.js
@@ -1512,7 +1512,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         self.stats.testAgentEnvId = MochaReporter.testAgentEnvId;
       }
 
-      console.log(self.stats);
       MochaReporter.send(JSON.stringify(['end', self.stats]));
     });
   }
@@ -2816,8 +2815,16 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     reporter: 'HTML',
 
     /**
-     * location of test helper.
+     * location of test helper(s).
+     *
      * Will be loaded before any of your tests.
+     * May pass more then one via an array.
+     *
+     * Each helper is loaded completely before
+     * requiring any other helpers allowing multiple
+     * files to be requested prior to executing tests.
+     *
+     * @type {String|Array}
      */
     testHelperUrl: './test/helper.js',
 
@@ -2858,6 +2865,29 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       return result;
     },
 
+    _loadTestHelpers: function(box, callback) {
+      var helpers = this.testHelperUrl;
+      if (typeof(helpers) === 'string') {
+        helpers = [helpers];
+      }
+
+      var current = 0;
+      var max = helpers.length;
+
+      function next() {
+        if (current < max) {
+          box.require(helpers[current], function() {
+            current++;
+            next();
+          });
+        } else {
+          callback();
+        }
+      }
+
+      next();
+    },
+
     _testRunner: function _testRunner(worker, tests, done) {
       var box = worker.sandbox.getWindow(),
           self = this;
@@ -2883,7 +2913,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         });
       });
 
-      box.require(this.testHelperUrl, function(){
+      self._loadTestHelpers(box, function() {
         tests.forEach(function(test) {
           box.require(test);
         });
